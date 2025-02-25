986071ea9dd96d975e3bc35ba289d5c0d48d2151
sangyoon kim <sangyun0904@gmail.com>
2025-02-25 19:24:04 +0900

    Add project

---
+++ src/test/java/com/example/CodeJuniorBackendEngineer/CodeJuniorBackendEngineerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CodeJuniorBackendEngineer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeJuniorBackendEngineerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
